3161ab10d37918cd1efc013f3925158d9be535dc
seyfer <seyferseed@mail.ru>
2013-12-10 14:10:16 +0800

    Refactoring. Using composer for mPDF loading.

--- classes/kohana/mpdf.php
+++ classes/kohana/mpdf.php
@@ -2,6 +2,11 @@
 
 defined('SYSPATH') or die('No direct script access.');
 
+define('_MPDF_PATH', MODPATH . 'mpdf/vendor/mPDF/');
+
+// Load mPDF configuration, this will prepare mPDF
+require_once _MPDF_PATH . 'mpdf.php';
+
 /**
  * Kohana wrapper on MPDF.
  *
@@ -11,38 +16,109 @@ defined('SYSPATH') or die('No direct script access.');
  */
 class Kohana_MPDF {
 
-    private $mpdf;
-    private $data;
-    private $file;
-    private $css = array();
-    private $charset;
-    private $format;
-    private $default_font_size;
+    const WRITE_DEFAULT      = 0;
+    const WRITE_CSS          = 1;
+    const WRITE_HTML_BODY    = 2;
+    const WRITE_HTML_PARSES  = 3;
+    const WRITE_HTML_HEADERS = 4;
 
-    public function set_default_font_size($default_font_size)
+    /**
+     *
+     * @var \mPDF
+     */
+    private $mpdf;
+
+    /**
+     * data for tpl filling
+     * @var type
+     */
+    private $data;
+
+    /**
+     * array of css path's
+     * @var array
+     */
+    private $css = array();
+
+    /**
+     * charset for pdf
+     * @var type
+     */
+    private $charset;
+
+    /**
+     * format (A4, etc)
+     * @var type
+     */
+    private $format;
+
+    /**
+     *
+     * @var \Kohana_View
+     */
+    private $view;
+
+    /**
+     * font size in px
+     * @var type
+     */
+    private $defaultFontSize;
+
+    /**
+     * tpl path
+     * @var type
+     */
+    private $tplPath;
+
+    public function setData($data)
     {
-        $this->default_font_size = $default_font_size;
+        $this->data = $data;
+        return $this;
     }
 
-    public function set_charset($charset)
+    public function setFilePath($tplPath)
+    {
+        $this->tplPath = $tplPath;
+        return $this;
+    }
+
+    public function setDefaultFontSize($defaultFontSize)
+    {
+        $this->defaultFontSize = $defaultFontSize;
+        return $this;
+    }
+
+    public function setCharset($charset)
     {
         $this->charset = $charset;
+        return $this;
     }
 
-    public function set_format($format)
+    public function setFormat($format)
     {
         $this->format = $format;
+        return $this;
     }
 
-    public function set_css($css)
+    /**
+     * add one css path to array
+     * @param type $css
+     */
+    public function setCss($css)
     {
         $this->css[] = $css;
+        return $this;
+    }
+
+    public function getMpdf()
+    {
+        return $this->mpdf;
     }
 
     public function __construct($file = NULL, $data = NULL)
     {
-        $this->data = $data;
-        $this->file = $file;
+        $this->data    = $data;
+        $this->tplPath = $file;
     }
 
     /**
@@ -57,50 +133,145 @@ class Kohana_MPDF {
     }
 
     /**
-     * Render HTML with tpl
-     * @param type $file - tpl
-     * @return \mPDF
+     * init instance of mPDF wit settings
      */
-    public function render($file = NULL)
+    public function initMpdf()
     {
-        $file = $file ? $file : $this->file;
-
-        // Render the HTML normally
-        // Using View::factory for compability with Smarty and other modules.
-        $html = View::factory($file);
-
-        if (is_array($this->data))
-            foreach ($this->data as $name => $value)
-                $html->set($name, $value);
-        else
-            $html->set("data", $this->data);
-
-        $rhtml = $html->render();
-
-        $charset           = $this->charset ? $this->charset : Kohana::$charset;
-        $format            = $this->format ? $this->format : 'A4';
-        $default_font_size = $this->default_font_size ? $this->default_font_size : "8";
+        $charset         = $this->charset ? $this->charset : Kohana::$charset;
+        $format          = $this->format ? $this->format : 'A4';
+        $defaultFontSize = $this->defaultFontSize ? $this->defaultFontSize : "8";
 
         //Create
-        $this->mpdf = new mPDF($charset, $format, $default_font_size, 10, 10, 7, 7, 10, 10);
+        $this->mpdf = new mPDF($charset, $format, $defaultFontSize, 10, 10, 7, 7, 10, 10);
 
-        //Set css
-        if (!empty($this->css))
-            foreach ($this->css as $css)
-            {
-                $stylesheet = file_get_contents($css);
+        return $this;
+    }
 
-                $this->mpdf->WriteHTML($stylesheet, 1);
+    /**
+     * init view for html render
+     * @param type $tplPath
+     * @return \Kohana_MPDF
+     * @throws Exception
+     */
+    public function initView($tplPath = NULL)
+    {
+        if ($tplPath) {
+            $this->setFilePath($tplPath);
+        }
+
+        if (!$this->tplPath) {
+            throw new Exception(__METHOD__ . " set tpl path first");
+        }
+
+        $this->view = View::factory($this->tplPath);
+
+        return $this;
+    }
+
+    /**
+     * set data to view for rendering
+     * @param type $data
+     */
+    public function setDataToView($data = array())
+    {
+        if ($data) {
+            $this->setData($data);
+        }
+
+        if (is_array($this->data) && !empty($this->data)) {
+            foreach ($this->data as $name => $value) {
+                $this->view->set($name, $value);
             }
+        }
+        else if (!empty($this->data)) {
+            $this->view->set("data", $this->data);
+        }
+    }
 
+    /**
+     * render current view
+     * @return type
+     * @throws Exception
+     */
+    public function renderView()
+    {
+        if (!$this->view) {
+            throw new Exception(__METHOD__ . " set view first");
+        }
+
+        $rhtml = $this->view->render();
+
+        return $rhtml;
+    }
+
+    /**
+     * set html to pdf
+     * @param type $rhtml
+     * @return \Kohana_MPDF
+     */
+    public function setDataToPdf($rhtml)
+    {
         //default preferences
         $this->mpdf->SetAutoFont(AUTOFONT_ALL);
         $this->mpdf->list_indent_first_level = 0;
 
         // Render the HTML to a PDF
-        $this->mpdf->WriteHTML($rhtml, 2);
+        $this->mpdf->WriteHTML($rhtml, Kohana_MPDF::WRITE_HTML_BODY);
 
-        return $this->mpdf;
+        return $this;
+    }
+
+    /**
+     * Render HTML with tpl
+     * @param type $file - tpl
+     * @return \Kohana_MPDF
+     */
+    public function render($tplPath = NULL)
+    {
+        if ($tplPath) {
+            $this->setFilePath($tplPath);
+        }
+
+        // Render the HTML normally
+        // Using View::factory for compability with Smarty and other modules.
+        $this->initView();
+
+        $this->setDataToView();
+
+        $rhtml = $this->renderView();
+
+        if (!$this->mpdf) {
+            $this->initMpdf();
+        }
+
+        //Set css
+        $this->setCssToPdf();
+
+        // Render the HTML to a PDF
+        $this->setDataToPdf($rhtml);
+
+        return $this;
+    }
+
+    /**
+     * set array of css path to pdf
+     * if exist
+     */
+    public function setCssToPdf($css = null)
+    {
+        if ($css && is_array($css)) {
+            $this->css = $css;
+        }
+
+        if (!empty($this->css) && is_array($this->css)) {
+            foreach ($this->css as $css) {
+                $stylesheet = file_get_contents($css);
+
+                $this->mpdf->WriteHTML($stylesheet, Kohana_MPDF::WRITE_CSS);
+            }
+        }
+
+        return $this;
     }
 
     /**
@@ -119,8 +290,7 @@ class Kohana_MPDF {
      */
     public function __call($name, $arguments)
     {
-        if (method_exists($this->mpdf, $name))
-        {
+        if (method_exists($this->mpdf, $name)) {
             return call_user_func_array(array($this->mpdf, $name), $arguments);
         }
     }
@@ -128,6 +298,4 @@ class Kohana_MPDF {
 }
 
 // End Kohana_MPDF
-// Load mPDF configuration, this will prepare mPDF
-require_once MODPATH . 'mpdf/vendor/mpdf.php';
-?>
+
